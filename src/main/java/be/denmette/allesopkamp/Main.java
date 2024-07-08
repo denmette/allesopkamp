084cd011cd1ee55e39b474f0a1ddc4f24e14774a
Maarten Casteels <denmette@gmail.com>
2024-07-08 21:21:50 +0200

    feat: 🤣 Add vaadin to the project.

--- src/main/java/be/denmette/allesopkamp/Main.java
+++ src/main/java/be/denmette/allesopkamp/Main.java
@@ -1,10 +1,14 @@
 package be.denmette.allesopkamp;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+@Theme(value = "allesopkamp")
+public class Main implements AppShellConfigurator {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
---
+++ src/main/java/be/denmette/allesopkamp/ui/members/MemberListView.java
@@ -0,0 +1,34 @@
+package be.denmette.allesopkamp.ui.members;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@PageTitle("Members")
+@Route
+@RouteAlias(value = "")
+public class MemberListView extends VerticalLayout {
+
+    public MemberListView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(LumoUtility.Margin.Top.XLARGE, LumoUtility.Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+}
