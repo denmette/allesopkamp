9a5e6b40950566cc7fa1bd4ea96f61fdb72cbfdf
Maarten Casteels <denmette@gmail.com>
2024-10-26 06:51:51 +0200

    feat: start fresh

---
+++ src/main/java/be/denmette/allesopkamp/Application.java
@@ -0,0 +1,13 @@
+package be.denmette.allesopkamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- src/main/java/be/denmette/allesopkamp/vaadin/data/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package be.denmette.allesopkamp.vaadin.data;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-
-}
